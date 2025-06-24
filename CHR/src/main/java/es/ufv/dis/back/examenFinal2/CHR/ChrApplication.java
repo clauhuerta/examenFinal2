11272062de1684e0c70101ccd06f5a28f678b52d
clauhuerta <claudiahr02@icloud.com>
2025-06-24 21:48:16 +0200

    añadido carpetas back y front

---
+++ CHR/src/main/java/es/ufv/dis/back/examenFinal2/CHR/ChrApplication.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.back.examenFinal2.CHR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChrApplication.class, args);
+	}
+
+}
---
+++ CHR/src/test/java/es/ufv/dis/back/examenFinal2/CHR/ChrApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.back.examenFinal2.CHR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
